95f8909548fd9b8c80c987a9457ccc9ed8352698
Guruhubgit <90470020+Guruhubgit@users.noreply.github.com>
2022-06-17 14:34:16 +0530

    Update repout.groovy

--- vars/repout.groovy
+++ vars/repout.groovy
@@ -1,7 +1,7 @@
 def call(String git_repo_url="", CredentialId=""){
-    echo "Cloning started "
+    echo "Cloning started"
      checkout(
                 [$class: 'GitSCM', branches: [[name: '*/master']],
-                 userRemoteConfigs: [[credentialsId: '$CredentialId' url: '$git_repo_url']]],
+                 userRemoteConfigs: [[credentialsId: '$CredentialId', url: '$git_repo_url']]],
      )
 }
