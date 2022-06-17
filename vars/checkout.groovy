a0c813feac8923afc6f01f06303acd8f97f14d67
Guruhubgit <90470020+Guruhubgit@users.noreply.github.com>
2022-06-17 14:15:57 +0530

    Delete checkout.groovy

--- vars/checkout.groovy
+++
@@ -1,8 +0,0 @@
-def call (String git_repo_url="", String git_cred="", String branch="")
-{
-    echo "Cloning started "
-    checkout(
-                [$class: 'GitSCM', branches: [[name: */master]],
-                 userRemoteConfigs: [[credentialsId: '$git_cred',url: '$git_repo_url']]], 
-             )
-}
