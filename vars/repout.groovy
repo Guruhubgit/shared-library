9ef9d9689ceccfb0c7c7dbb4d62204697e569380
Guruhubgit <90470020+Guruhubgit@users.noreply.github.com>
2022-06-17 15:33:48 +0530

    Update repout.groovy

--- vars/repout.groovy
+++ vars/repout.groovy
@@ -1,8 +1,7 @@
-def call(String git_repo_url="https://github.com/Guruhubgit/shared-library.git", String CredentialId="mine"){
+def call(def giturl, def branch ){
     echo "Cloning started"
-    echo "$git_repo_url"
      checkout(
-                [$class: 'GitSCM', branches: [[name: '*/master']],
-                 userRemoteConfigs: [[credentialsId: '$CredentialId', url: '$git_repo_url']]],
+                [$class: 'GitSCM', branches: [[name: '*/branch']],
+                 userRemoteConfigs: [[url: 'giturl']]],
      )
 }
