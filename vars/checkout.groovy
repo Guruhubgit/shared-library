150eb846fbd1ca65a11a41dd66cd2b7edb57250f
Guruhubgit <90470020+Guruhubgit@users.noreply.github.com>
2022-06-17 13:16:26 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -4,7 +4,7 @@ def call (String git_repo_url="", String git_cred="", String git_branch="")
     echo "Cloning started "
 
     checkout(
-                [$class: 'GitSCM', branches: [[name: '*/$git_branch']],
+                [$class: 'GitSCM', branches: [[name: */'$git_branch']],
                  userRemoteConfigs: [[credentialsId: '$git_cred',url: '$git_repo_url']]],
 
         )
